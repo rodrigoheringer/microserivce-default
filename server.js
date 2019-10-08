cb3dd1c5eff59275b256640636152b8669271978
Rodrigo Heringer <heringer@br.ibm.com>
2019-10-08 19:20:22 -0300

    default router

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const enableApiDocs = require('./default/api-docs');
 const configureLogger = require('./default/log/configure-logger');
 const { errorHandler } = require('./util/error-handler');
 
-const createServer = ({package}) => {
+const createServer = ({ package, defaultRouter, defaultRouterPath }) => {
     enableApiDocs({server, package});
     server.use(bodyParser.json());
     configureLogger(server, environment.TRACE_HEADER, environment.TRACE_UNIQUE_HEADER_ID, environment.LOG_LEVEL);
-    server.use(environment.DEFAULT_INFO_ROUTE, createDefaultApi({package}));
+    server.use(environment.DEFAULT_INFO_ROUTE, createDefaultApi({ package }));
+    server.use(defaultRouterPath, defaultRouter);
     server.use(errorHandler);
     server.listen(environment.API_PORT, undefined, () => {
         console.log(`listening on port ${environment.API_PORT}`);
