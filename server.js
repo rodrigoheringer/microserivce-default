655740906b539ff47f45ea0e18be74cfa488e89c
Rodrigo Heringer <heringer@br.ibm.com>
2019-10-08 13:12:45 -0300

    createServer clean up

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const server = express();
-// const routes = require('./api/contract/route');
 const bodyParser = require('body-parser');
 const environment = require('../environment');
 const defaults = require('./default');
 const enableApiDocs = require('./default/api-docs');
 const configureLogger = require('./default/log');
 const { errorHandler } = require('./util/error-handler');
-// const package = require('../package');
 
 const createServer = () => {
 
-    // const { name, version } = package;
-    // const basePath = `/${name}/v${version.split('.')[0]}`;
     enableApiDocs(server);
     server.use(bodyParser.json());
     configureLogger(server, environment.TRACE_HEADER, environment.TRACE_UNIQUE_HEADER_ID, environment.LOG_LEVEL);
-    // server.use(basePath, routes);
     server.use('/info', defaults.defaultApi);
     server.use(errorHandler);
     server.listen(environment.API_PORT, undefined, () => {
